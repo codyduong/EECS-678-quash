d00f41544433fa5de2b5cb088961e5ec0f5da3c3
Cody Duong <duongcody507@gmail.com>
2024-03-06 15:24:48 -0600

    Implement get_current_directory

--- src/execute.c
+++ src/execute.c
@@ -26,14 +26,13 @@
 
 // Return a string containing the current working directory.
 char* get_current_directory(bool* should_free) {
-  // TODO: Get the current working directory. This will fix the prompt path.
-  // HINT: This should be pretty simple
-  IMPLEMENT_ME();
-
-  // Change this to true if necessary
   *should_free = false;
-
-  return "get_current_directory()";
+  char cwd[1024];
+  if (getcwd(cwd, sizeof(cwd)) != NULL) {
+    *should_free = true;
+    return strdup(cwd);
+  } 
+  return "getcwd() error";
 }
 
 // Returns the value of an environment variable env_var
